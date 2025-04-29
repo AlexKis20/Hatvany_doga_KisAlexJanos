d4a4ee4c21d2ff73c815203040091878541248b4
kisrondox5@gmail.com <kisrondox5@gmail.com>
2025-04-29 10:48:01 +0200

    rendezés

--- HatvanyTeszt/PowerCalculatorTests.cs
+++ HatvanyTeszt/PowerCalculatorTests.cs
@@ -9,10 +9,10 @@ namespace HatvanyTeszt
         [TestMethod]
         public void TestPositiveExponent()
         {
-
+            double eredmeny = new PowerCalculator();
             double a = 2;
             double b = 3;
-            double eredmeny = new PowerCalculator();
+            
             
             
 
@@ -22,10 +22,10 @@ namespace HatvanyTeszt
         [TestMethod]
         public void TestZeroExponent()
         {
-
+            double eredmeny = new PowerCalculator();
             double a = 5;
             double b = 0;
-            double eredmeny = new PowerCalculator();
+            
 
 
 
@@ -35,10 +35,10 @@ namespace HatvanyTeszt
         [TestMethod]
         public void TestNegativeExponent()
         {
-
+            double eredmeny = new PowerCalculator();
             double a = 2;
             double b = -2;
-            double eredmeny = new PowerCalculator();
+            
 
 
 
@@ -48,10 +48,10 @@ namespace HatvanyTeszt
         [TestMethod]
         public void TestZeroBasePositiveExponent()
         {
-
+            double eredmeny = new PowerCalculator();
             double a = 0;
             double b = 5;
-            double eredmeny = new PowerCalculator();
+            
 
 
 
@@ -61,10 +61,10 @@ namespace HatvanyTeszt
         [TestMethod]
         public void TestZeroBaseZeroExponent()
         {
-
+            double eredmeny = new PowerCalculator();
             double a = 0;
             double b = 0;
-            double eredmeny = new PowerCalculator();
+            
 
 
 
@@ -74,10 +74,10 @@ namespace HatvanyTeszt
         [TestMethod]
         public void TestOneBaseAnyExponent()
         {
-
+            double eredmeny = new PowerCalculator();
             double a = 1;
             double b = 100;
-            double eredmeny = new PowerCalculator();
+            
 
 
 
@@ -87,10 +87,10 @@ namespace HatvanyTeszt
         [TestMethod]
         public void TestNegativeBaseEvenExponent()
         {
-
+            double eredmeny = new PowerCalculator();
             double a = -3;
             double b = 4;
-            double eredmeny = new PowerCalculator();
+            
 
 
 
@@ -100,10 +100,10 @@ namespace HatvanyTeszt
         [TestMethod]
         public void TestNegativeBaseOddExponent()
         {
-
+            double eredmeny = new PowerCalculator();
             double a = -2;
             double b = 3;
-            double eredmeny = new PowerCalculator();
+            
 
 
 
@@ -113,10 +113,10 @@ namespace HatvanyTeszt
         [TestMethod]
         public void TestNegativeBaseNegativeExponent()
         {
-
+            double eredmeny = new PowerCalculator();
             double a = -2;
             double b = -2;
-            double eredmeny = new PowerCalculator();
+            
 
 
 
